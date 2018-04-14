5c41092ea76947dedb2b748d2c97e409af939df0
Pyrov Sergey <pyrov.sergey@gmail.com>
2018-04-14 20:00:41 +0300

    Initial commit

---
+++ app/src/main/java/test/com/simplebroadcast/MainActivity.java
@@ -0,0 +1,25 @@
+package test.com.simplebroadcast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String WHERE_MY_CAT_ACTION = "ru.alexanderklimov.action.CAT";
+    public static final String ALARM_MESSAGE = "Срочно пришлите кота!";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent();
+        intent.setAction(WHERE_MY_CAT_ACTION);
+        intent.putExtra("ru.alexanderklimov.broadcast.Message", ALARM_MESSAGE);
+        //intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(intent);
+    }
+}
