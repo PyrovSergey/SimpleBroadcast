f37d891c1a3d2cd9a21041f75655ab72bf34c17e
Pyrov Sergey <pyrov.sergey@gmail.com>
2018-04-15 16:59:28 +0300

    update_1

--- app/src/main/java/test/com/simplebroadcast/MainActivity.java
+++ app/src/main/java/test/com/simplebroadcast/MainActivity.java
@@ -1,13 +1,17 @@
 package test.com.simplebroadcast;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String WHERE_MY_CAT_ACTION = "ru.alexanderklimov.action.CAT";
     public static final String ALARM_MESSAGE = "Срочно пришлите кота!";
+
+    private TimeBroadcastReceiver mTimeBroadCastReceiver = new TimeBroadcastReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,24 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent();
         intent.setAction(WHERE_MY_CAT_ACTION);
         intent.putExtra("ru.alexanderklimov.broadcast.Message", ALARM_MESSAGE);
-        //intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
         sendBroadcast(intent);
+    }
+
+    // регистрируем широковещательный приёмник
+    // для намерения "android.intent.action.TIME_TICK".
+    // Данное намерение срабатывает каждую минуту
+    public void registerBroadcastReceiver(View view) {
+        this.registerReceiver(mTimeBroadCastReceiver, new IntentFilter(
+                "android.intent.action.TIME_TICK"));
+        Toast.makeText(getApplicationContext(), "Приёмник включен",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    // Отменяем регистрацию
+    public void unregisterBroadcastReceiver(View view) {
+        this.unregisterReceiver(mTimeBroadCastReceiver);
+        Toast.makeText(getApplicationContext(), "Приёмник выключён", Toast.LENGTH_SHORT)
+                .show();
     }
 }
--- app/src/main/java/test/com/simplebroadcast/MessageReceiver.java
+++ app/src/main/java/test/com/simplebroadcast/MessageReceiver.java
@@ -9,8 +9,29 @@ public class MessageReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Обнаружено сообщение: " +
-                        intent.getStringExtra("ru.alexanderklimov.broadcast.Message"),
-                Toast.LENGTH_LONG).show();
+//        Toast.makeText(context, "Обнаружено сообщение: " +
+//                        intent.getStringExtra("ru.alexanderklimov.broadcast.Message"),
+//                Toast.LENGTH_LONG).show();
+        if (intent.getAction().equalsIgnoreCase("android.intent.action.ACTION_POWER_DISCONNECTED")) {
+            String message = "Обнаружено сообщение "
+                    + intent.getAction();
+
+            Toast.makeText(context, message,
+                    Toast.LENGTH_LONG).show();
+        }
+
+        if (intent.getAction().equalsIgnoreCase("android.intent.action.ACTION_POWER_CONNECTED")) {
+            String message = "Обнаружено сообщение "
+                    + intent.getAction();
+
+            Toast.makeText(context, message,
+                    Toast.LENGTH_LONG).show();
+        }
+
+        if (intent.getAction().equalsIgnoreCase("ru.alexanderklimov.action.CAT")) {
+            Toast.makeText(context, "Обнаружено сообщение: " +
+                            intent.getStringExtra("ru.alexanderklimov.broadcast.Message"),
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
